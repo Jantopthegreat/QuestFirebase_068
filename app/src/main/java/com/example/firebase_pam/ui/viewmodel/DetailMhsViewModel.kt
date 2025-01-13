eda9166dd6dfe6267d8109a9b97e1158d2a117a6
Jantopthegreat <fauzangriya@gmail.com>
2025-01-13 12:57:09 +0700

    Membuat Detail View Model

---
+++ app/src/main/java/com/example/firebase_pam/ui/viewmodel/DetailMhsViewModel.kt
@@ -0,0 +1,61 @@
+package com.example.firebase_pam.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.firebase_pam.model.Mahasiswa
+import com.example.firebase_pam.repository.MahasiswaRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+
+
+sealed class DetailMhsUiState {
+    data class Success(val mahasiswa: Mahasiswa) : DetailMhsUiState()
+    object Error : DetailMhsUiState()
+    object Loading : DetailMhsUiState()
+}
+
+class DetailMhsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val mahasiswaRepository: MahasiswaRepository
+) : ViewModel() {
+
+    private val nim: String = checkNotNull(savedStateHandle["nim"]) // Ambil nim dari savedState
+
+    // Menggunakan StateFlow untuk detailMhsUiState
+    private val _detailMhsUiState = MutableStateFlow<DetailMhsUiState>(DetailMhsUiState.Loading)
+    val detailMhsUiState: StateFlow<DetailMhsUiState> = _detailMhsUiState
+
+    init {
+        getMhsbyNim()
+    }
+
+    fun getMhsbyNim() {
+        viewModelScope.launch {
+            _detailMhsUiState.value = DetailMhsUiState.Loading // Menampilkan loading sebelum data diambil
+
+            mahasiswaRepository.getMahasiswaByNim(nim)
+                .onStart {
+                    // Menunggu data dari Firestore
+                }
+                .catch {
+                    // Tangani kesalahan
+                    _detailMhsUiState.value = DetailMhsUiState.Error
+                }
+                .collect { mahasiswa ->
+                    _detailMhsUiState.value = DetailMhsUiState.Success(mahasiswa) // Jika berhasil, update UI
+                }
+        }
+    }
+}
